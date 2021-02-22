f0fef21f1d0d83ce8594d633271db5b818243e5e
David Cabiness <dcabiness@werktek.com>
2021-02-22 10:12:51 -0500

    trying more stuff

--- src/routes/other/test-post/index.svelte
+++ src/routes/other/test-post/index.svelte
@@ -1,6 +1,18 @@
+<script>
+import Thing from '../../../components/Thing.svelte';
+
+let user = {
+    firstname: "Ada",
+    lastname: "Lovelace",
+  };
+</script>
+
 <svelte:head>
-	<title>Example</title>
+  <title>Example</title>
 </svelte:head>
 
 <h1>This is an example</h1>
 <p>I hope it works.</p>
+
+<h1>Hello {user.firstname}!</h1>
+<Thing/>
