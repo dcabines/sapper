b7ca17904032592485ecb97c134117e578a24988
David Cabiness <dcabiness@werktek.com>
2021-02-23 10:05:49 -0500

    added todo

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,7 +1,19 @@
+<script>
+  import Todos from "../components/Todos.svelte";
+
+  let todos = [
+    { id: 1, name: "Create a Svelte starter app", completed: true },
+    { id: 2, name: "Create your first component", completed: true },
+    { id: 3, name: "Complete the rest of the tutorial", completed: false },
+  ];
+</script>
+
 <svelte:head>
-	<title>About</title>
+  <title>About</title>
 </svelte:head>
 
 <h1>About this site</h1>
 
 <p>This is the 'about' page. There's not much here. I made a change.</p>
+
+<Todos {todos} />
